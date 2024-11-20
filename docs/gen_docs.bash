2f512a8321d69423288d41fe465de7dc7817a919
Allison Piper <alliepiper16@gmail.com>
2024-11-20 20:42:08 +0000

    Migrate gen_docs.bash away from repo_man.

--- docs/gen_docs.bash
+++ docs/gen_docs.bash
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-# This script just wraps launching a repo docs build within a container
-#
-# Additional options, e.g --stage sphinx will be passed on to repo.sh
+set -euo pipefail
 
-set -e
+readonly script_path=$(cd $(dirname ${0}); pwd -P)
+readonly docs_build_path=../build/docs
+readonly image_path=${docs_build_path}/img
+readonly cub_docs_repo=${docs_build_path}/cubimg
 
-SCRIPT_PATH=$(cd $(dirname ${0}); pwd -P)
+cd ${script_path}
 
-cd $SCRIPT_PATH
+# Install prerequisites
+# sudo apt update
+sudo apt install -y --no-install-recommends doxygen
+pip install sphinx breathe nvidia-sphinx-theme
 
 ## Clean image directory, without this any artifacts will prevent fetching
-rm -rf img
-mkdir -p img
+rm -rf "${image_path}"
+mkdir -p "${image_path}"
 
 # Pull cub images
-if [ ! -d cubimg ]; then
-    git clone -b gh-pages https://github.com/NVlabs/cub.git cubimg
+if [ ! -d "${cub_docs_repo}" ]; then
+    git clone --depth 1 -b gh-pages https://github.com/NVlabs/cub.git "${cub_docs_repo}"
 fi
 
-if [ ! -n "$(find cubimg -name 'example_range.png')" ]; then
-    wget -q https://raw.githubusercontent.com/NVIDIA/NVTX/release-v3/docs/images/example_range.png -O cubimg/example_range.png
+if [ ! -n "$(find ${cub_docs_repo} -name 'example_range.png')" ]; then
+    wget -q https://raw.githubusercontent.com/NVIDIA/NVTX/release-v3/docs/images/example_range.png -O "${cub_docs_repo}/example_range.png"
 fi
 
-if [ ! -n "$(find img -name '*.png')" ]; then
-    wget -q https://docs.nvidia.com/cuda/_static/Logo_and_CUDA.png -O img/logo.png
+if [ ! -n "$(find ${image_path} -name '*.png')" ]; then
+    wget -q https://docs.nvidia.com/cuda/_static/Logo_and_CUDA.png -O "${image_path}/logo.png"
 
     # Parse files and collects unique names ending with .png
     imgs=( $(grep -R -o -h '[[:alpha:][:digit:]_]*.png' ../cub/cub | uniq) )
@@ -33,11 +37,22 @@ if [ ! -n "$(find img -name '*.png')" ]; then
     for img in "${imgs[@]}"
     do
         echo ${img}
-        cp cubimg/${img} img/${img}
+        cp "${cub_docs_repo}/${img}" "${image_path}/${img}"
     done
 fi
 
-if ! ./repo.sh docs "$@"; then
-    echo "!!! There were errors while generating"
-    exit 1
-fi
+
+# Generate doxygen xml output for sphinx/breathe:
+run_doxygen() {
+    local project=$1
+    local project_dir="${docs_build_path}/${project}"
+    echo "Generating doxygen xml for ${project}"
+    mkdir -p "${project_dir}/doxygen/xml"
+    doxygen doxyfiles/${project}.conf | tee "${project_dir}/doxygen/log.txt
+}
+
+run_doxygen cub
+run_doxygen cudax
+run_doxygen thrust
+
+sphinx-build -M html . "${docs_build_path}/sphinx" | tee "${docs_build_path}/sphinx/log.txt"
